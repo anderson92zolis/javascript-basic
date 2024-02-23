086a59082d1bf30f0bc843dbf87715c0f6755167
anderson92zolis <azolis92@hotmail.es>
2024-02-23 13:01:11 +0100

    adding files, script, consoles, alerts, operacion, button

---
+++ js/script.js
@@ -0,0 +1,57 @@
+
+
+let primeraVariable = "buenos días hora de break"; // Declarar variable con un valor Una vez creada el let con esta nombre de variable ya no se puede crear.
+var segundaVariable = "Son las nueve;"; //Declarar variable con un valor y tipo de dato (caracter)
+terceraVariable = "Adios"; // asignación de la variable sin asignar el let or var , esto no es aconsejable
+const PI = 3.1416; // Variable Constante que no puede ser reasignada PALABRA MAYUSCULA
+
+console.log(segundaVariable + " SE IMPRIRÁ POR VARIABLE");
+//   alert(primeraVariable);
+
+let palabra = "H" + "O" + "L" + "L" + "A"; // Suma de caracteres  en JavaScript dan un String
+
+// saber el tipo de variable
+
+console.log(typeof(PT));
+
+// creación de una function:
+
+
+var tituloInicial = document.getElementById("demo").textContent ;
+function changeName() {
+  //Modificando el contenido del elemento HTML por medio de la función "innerHTML
+
+  // Seleccionar el elemento h1 por su ID
+
+  titulo = document.getElementById("demo").innerHTML = "¡Nuevo título cambiado!";
+}
+  
+
+// <button type="button" onclick="document.getElementById('demo').innerHTML= 'My first  JavaScript'">Click Me!</button>
+
+// document.getElementById("demo").style.fontSize = "100px";
+
+let segunda_variable= 1;
+let nombre= "L' avinguda " + " Ferran"
+
+// alert(nombre);
+
+
+
+
+console.log(segundaVariable);
+
+
+// pide un nombre por ventana y da unn mensaje en ventana
+nombre = prompt("Cúal es tu nombre");
+alert("Saludos" +nombre);
+
+
+//confirm para respuestas vinarias  Sí o No
+
+let llueve = confirm("Está lluviendo ahora!!");
+alert(llueve);
+
+
+
+
---
+++ js/suma.js
@@ -0,0 +1,13 @@
+//  creamos casting para que tranformar desde un string a integer
+
+let numero1= parseInt(prompt("Introduce número 1")) ;
+
+let numero2= parseInt(prompt("Introduce número 2"));
+
+let suma= numero1 +numero2 
+
+//alert("La suma total es : "+ suma)
+//alerta("la suma total es: " + (numero1 +numero2)) // otra manera de hacerlo
+
+
+alert(` La suma ES ${numero1 +numero2}`) // otra manera de mostrar por ventana
